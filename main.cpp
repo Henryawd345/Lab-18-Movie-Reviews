14f9c07cafb478be8541acd23a3670cbef231688
Henryawd345 <henrygoodboy2000@gmail.com>
2025-10-05 00:43:35 -0700

    update

--- main.cpp
+++ main.cpp
@@ -2,13 +2,27 @@
 using namespace std;
 
 struct Node{
-    float value;
+    float rating;
+    string comment;
     Node *next;
 };
 
-Node* insertHead(){
+Node* insertHead(Node *head, float number, string comment){
+    Node *head = nullptr;
 
-    return NULL;
+    for (int i = 0; i < 3; i++){
+        Node *newVal = new Node;
+
+        if (!head) {
+            head = newVal;
+            newVal->next = nullptr;
+        }
+        else {
+            newVal->next = head;
+            head = newVal;
+        }
+    }
+    return head;
 }
 
 Node* insertTail(){
