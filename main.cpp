948aef73df4788297ccc16d3d778f8b7bdf47a9e
Henryawd345 <henrygoodboy2000@gmail.com>
2025-10-05 01:39:57 -0700

    update

--- main.cpp
+++ main.cpp
@@ -23,17 +23,37 @@ Node* insertHead(Node *head, float numrate, string comment){
 }
 
 Node* insertTail(Node *head, float numrate, string comment){
-
-
-
+    Node* newNode = new Node;
+    newNode->rating = numrate;
+    newNode->comment = comment;
     
-    return NULL;
+
+    if (head == nullptr){
+        head = newNode;     
+    }
+    else{
+        Node* curr = head;
+        while (curr->next != nullptr)
+        {
+            curr = curr->next;
+            curr->next = newNode;
+        }
+    }
+    return head;
 }
 
 int main(){
     Node *head = nullptr;
     int tmp;
     int choices;
+
+    cout<<"Which linked list method should we use?"<<"\n";
+    cout<<"[1] New nodes are added at the head of the linked list"<<"\n";
+    cout<<"[2] New nodes are added at the tail of the linked list"<<"\n";
+    cout<<"Choices: ";
+    cin>> choices ;
+    cin.ignore();
+
 
     for (int i = 0; i < 3; i++){
     
